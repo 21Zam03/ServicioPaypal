44fa31689b6817752f878ab51b944e59de701a4a
jose <21jose03.free@gmail.com>
2024-02-25 16:15:38 -0500

    eliminar el archivo homecontroller

--- src/main/java/com/zam/servicioPayPal/controller/HomeController.java
+++
@@ -1,9 +0,0 @@
-package com.zam.servicioPayPal.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-}
